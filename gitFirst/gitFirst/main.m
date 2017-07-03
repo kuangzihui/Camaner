32e1cdb2da32bc2bb66d051e39591e8457fdfe9e
kuangzihui <kuangzihui@163.com>
2017-07-03 15:22:11 +0800

    第二次提交

---
+++ gitFirst/gitFirst/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitFirst
+//
+//  Created by ejt_ios on 2017/7/3.
+//  Copyright © 2017年 ejt_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
